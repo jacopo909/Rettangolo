921141c98fb94181e7239871ba51e21e6842c253
jacopo909 <60032905+jacopo909@users.noreply.github.com>
2020-10-06 23:14:08 +0200

    Add files via upload

---
+++ Rettangolo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rettangolo
+{
+    public class Rettangolo 
+    {
+        
+
+        double x;
+        double y;
+        double base_;
+        double altezza;
+        public Rettangolo(double x, double y,double B,double H)
+        {
+            this.x = x;
+            this.y = y;
+            base_ = B;
+            altezza = H;
+        }
+        public double CalcolaArea()
+        {
+            return  base_ * altezza; 
+        }
+        public double CalcolaPerimetro()
+        {
+            return (base_ + altezza) * 2;
+        }
+        public bool Contiene()
+        {
+            double[] rettangolo = new double[1];
+            foreach (int j in rettangolo)
+            {
+                double[] rettangolo1 = new double[1];
+                rettangolo1[0] = CalcolaArea();
+                rettangolo1[1] = CalcolaPerimetro();
+
+            }
+            if(rettangolo[0] < rettangolo[1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+          
+        }
+              
+    }
+    
+    
+}
